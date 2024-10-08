e182a860e6ccf966e235adbe2647466e983f6ca6
Kilowon <shauncameron.inbox.com>
2024-10-08 15:50:22 -0400

    add: solidquery & tools

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,35 @@
-import "@unocss/reset/tailwind.css";
-import "virtual:uno.css";
+import "@unocss/reset/tailwind.css"
+import "virtual:uno.css"
 
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
+import { Router } from "@solidjs/router"
+import { FileRoutes } from "@solidjs/start/router"
+import { Suspense } from "solid-js"
+import Nav from "~/components/Nav"
+import { SolidQueryDevtools } from "@tanstack/solid-query-devtools"
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query"
 
 export default function App() {
-  return (
-    <Router
-      root={(props) => (
-        <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				retry: false
+			}
+		}
+	})
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Router
+				root={props => (
+					<>
+						<Nav />
+						<Suspense>{props.children}</Suspense>
+					</>
+				)}
+			>
+				<FileRoutes />
+			</Router>
+			<SolidQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	)
 }
