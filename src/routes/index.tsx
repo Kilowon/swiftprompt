c2babcc712b8c97b86d3f0c5321eaa3dfcba69e2
Kilowon <shauncameron.inbox.com>
2024-10-08 16:11:16 -0400

    add: shortcut key color footer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,18 @@
+import { createShortcut } from "@solid-primitives/keyboard"
+import { setColorFooter } from "~/global_state"
+
 export default function Home() {
+	// Toggle Color Footer
+	createShortcut(
+		["Control", "Shift", "C"],
+		() => {
+			setColorFooter(prev => !prev)
+		},
+		{
+			preventDefault: true
+		}
+	)
+
 	return (
 		<main class="text-center mx-auto text-gray-700 p-4">
 			<div>Clear</div>
