eb5176f2d48f91539f3ad96f6b3f855926b75fde
Kilowon <shauncameron.inbox.com>
2024-10-19 11:52:21 -0400

    cont: element toggle

--- src/components/group-container-search.tsx
+++ src/components/group-container-search.tsx
@@ -1,4 +1,6 @@
 import { Accessor } from "solid-js"
+import { Tooltip, TooltipContent, TooltipTrigger } from "~/registry/ui/tooltip"
+import { Button } from "~/registry/ui/button"
 
 interface GroupContainerSearchProps {
 	setIsFullElements: (value: boolean) => void
@@ -6,7 +8,34 @@ interface GroupContainerSearchProps {
 }
 
 function GroupContainerSearch(props: GroupContainerSearchProps) {
-	return <div>GroupContainerSearch</div>
+	return (
+		<div class="p-4">
+			<div class="flex items-start justify-between">
+				<div>Search bar</div>
+				<div class="flex items-center gap-2">
+					<Tooltip
+						openDelay={1000}
+						closeDelay={0}
+					>
+						<TooltipTrigger
+							as={Button}
+							onClick={() => props.setIsFullElements(!props.isFullElements())}
+							variant="ghost"
+							size="icon"
+							class="h-9 ml-1"
+						>
+							{props.isFullElements() ? (
+								<div class="i-mdi:view-list w-1.25em h-1.25em "></div>
+							) : (
+								<div class="i-mdi:view-grid-outline w-1.25em h-1.25em"></div>
+							)}
+						</TooltipTrigger>
+						<TooltipContent>{props.isFullElements() ? "Toggle List Elements" : "Toggle Grid Elements"}</TooltipContent>
+					</Tooltip>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default GroupContainerSearch
