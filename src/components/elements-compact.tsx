e6160a132ab9019824a0c5ec5b371bf0fb3bbffc
Kilowon <shauncameron.inbox.com>
2024-10-21 23:49:02 -0400

    compact elements to container

--- src/components/elements-compact.tsx
+++ src/components/elements-compact.tsx
@@ -1,20 +1,132 @@
-import { PromptItem } from "~/types/entityType"
-import { Button } from "~/registry/ui/button"
+import { createEffect, Show, createSignal } from "solid-js"
+import { toast } from "solid-sonner"
 import { cn } from "~/lib/utils"
+import {
+	selectedItem,
+	setSelectedItem,
+	isEditingItem,
+	setIsEditingItem,
+	entityGroups,
+	selectedTemplateGroup,
+	selectedSection,
+	selectedSectionItem,
+	setSelectedSectionItemEl,
+	setSelected,
+	selectedSectionItemEl,
+	selectedTemplateVersion
+} from "~/global_state"
+import { pinToggleItem } from "~/helpers/actionHelpers"
+import { PromptItem, GroupID, ElementID, VersionID } from "~/types/entityType"
+import {
+	DropdownMenu,
+	DropdownMenuTrigger,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSub,
+	DropdownMenuSubTrigger,
+	DropdownMenuSubContent,
+	DropdownMenuPortal
+} from "~/registry/ui/dropdown-menu"
+import { Button } from "~/registry/ui/button"
+import { Tooltip, TooltipTrigger, TooltipContent } from "~/registry/ui/tooltip"
+import { Select, SelectTrigger, SelectContent, SelectItem, SelectValue } from "~/registry/ui/select"
+import { addItemToTemplateSection } from "~/helpers/actionHelpers"
 
-interface ItemsCompactProps {
+interface ElementsCompactProps {
 	item: PromptItem
+	handleEditing: (item: PromptItem, label: "title" | "summary" | "body", status: "editing" | "saved", id: string) => void
+	handleUpdateAttributes: (
+		item: PromptItem,
+		name: string,
+		summary: string,
+		body: string,
+		version: VersionID,
+		versionCounter: VersionID,
+		updatedBody: boolean
+	) => void
+	handleDeleteItem: (groupId: GroupID, itemId: ElementID) => void
+	handleDuplicateItem: (item: PromptItem) => void
+	handleMoveItem: (item: PromptItem, groupId: GroupID) => void
+	labelLimit: () => number
+	items: PromptItem[]
+	sizes: number[]
+	setIsFullElements: (value: boolean) => void
 }
 
-function ElementsCompact(props: ItemsCompactProps) {
+function ElementsCompact(props: ElementsCompactProps) {
+	const [isPinned, setIsPinned] = createSignal(false)
+	let el: HTMLButtonElement | undefined
+
+	createEffect(() => {
+		if (selectedSectionItem() === props.item.id) {
+			setSelectedSectionItemEl(el)
+		}
+	})
+
+	const groupOptions = Array.from(entityGroups.values())
+		.filter((group: any) => group.id !== props.item.group)
+		.map((group: any) => ({
+			value: group.id,
+			label: group.name
+		}))
+
+	createEffect(() => {
+		if (props.item.pinned) {
+			setIsPinned(true)
+		} else {
+			setIsPinned(false)
+		}
+	})
+
+	const handlePinToggle = () => {
+		pinToggleItem(props.item.group, props.item.id)
+		setIsPinned(props.item.pinned || false)
+	}
+
+	const handleNewGroup = (groupId: any) => {
+		props.handleMoveItem(props.item, groupId.value)
+	}
+
+	const handleAddToTemplate = () => {
+		addItemToTemplateSection(
+			selectedTemplateGroup()!,
+			selectedSection()!,
+			props.item.id,
+			props.item.group,
+			selectedTemplateVersion()!
+		)
+	}
+
+	const [isDebouncing, setIsDebouncing] = createSignal(false)
+	const handleDebounce = () => {
+		if (!isDebouncing()) {
+			setIsDebouncing(true)
+			handleAddToTemplate()
+			setTimeout(() => {
+				setIsDebouncing(false)
+			}, 1000)
+		}
+	}
+
+	const handleOpenItem = (itemId: ElementID, groupId: GroupID) => {
+		setSelected(groupId)
+		setSelectedItem(itemId)
+		props.setIsFullElements(true)
+		selectedSectionItemEl()?.focus()
+		setIsEditingItem({ status: "editing", id: itemId, label: "all" })
+	}
+
 	return (
 		<div>
 			<Button
+				ref={el}
 				variant="no_format"
 				size="no_format"
 				class={cn(
-					"flex relative  gap-2 rounded-md border border-border p-0.5 text-left text-sm transition-all hover:bg-muted/50 cursor-default max-h-10 min-h-10"
+					"flex relative  gap-2 rounded-md border border-border p-0.5 text-left text-sm transition-all hover:bg-muted/50 cursor-default max-h-10 min-h-10",
+					selectedItem() === props.item.id && "bg-muted/50 ring-1 border-accent ring-accent  transition-none"
 				)}
+				onClick={() => setSelectedItem(props.item.id)}
 			>
 				<div class="flex w-full flex-col gap-2 p-0.5">
 					<div class="flex flex-col items-start justify-between w-full relative">
@@ -37,7 +149,149 @@ function ElementsCompact(props: ItemsCompactProps) {
 				<div class="">
 					<div class="flex gap-1 items-center">
 						<div class="flex items-center gap-2 min-w-23 min-h-10">
-							<div>Buttons</div>
+							<Show when={isPinned() && props.sizes[1] > 0.2 && isEditingItem().status === "saved"}>
+								<Button
+									variant="ghost"
+									size="icon"
+									onClick={() => {
+										handlePinToggle()
+										toast(
+											<div class="flex items-center gap-2">
+												<div class="i-material-symbols:check-box w-4 h-4 text-success" />
+												<span class="text-xs font-medium">
+													{props.item.name} {isPinned() ? "Unpinned" : "Pinned"}
+												</span>
+											</div>,
+											{ duration: 2000, position: "bottom-center" }
+										)
+									}}
+									class="group"
+								>
+									<div class="i-material-symbols-light:push-pin text-foreground/80 w-4 h-4 group-hover:text-accent-foreground"></div>
+								</Button>
+							</Show>
+
+							<Show when={selectedItem() === props.item.id}>
+								<Tooltip
+									openDelay={1000}
+									closeDelay={0}
+								>
+									<TooltipTrigger
+										as={Button}
+										onClick={() => {
+											handleOpenItem(props.item.id, props.item.group)
+										}}
+										variant="ghost"
+										size="icon"
+										class=" text-accent hover:text-accent-foreground max-h-8"
+									>
+										<div class="i-mdi:file-edit w-4 h-4" />
+										<span class="sr-only">Edit Prompt</span>
+									</TooltipTrigger>
+									<TooltipContent>Edit Prompt</TooltipContent>
+								</Tooltip>
+							</Show>
+							<Show when={selectedItem() === props.item.id}>
+								<Tooltip
+									openDelay={1000}
+									closeDelay={0}
+								>
+									<TooltipTrigger
+										as={Button}
+										variant="ghost"
+										size="icon"
+										class="text-accent hover:text-accent-foreground max-h-8"
+										onClick={() => {
+											handleDebounce()
+										}}
+									>
+										<div class="i-mdi:file-document-arrow-right w-1.25em h-1.25em"></div>
+										<span class="sr-only">Add to Template</span>
+									</TooltipTrigger>
+									<TooltipContent>Add to Template</TooltipContent>
+								</Tooltip>
+							</Show>
+							<Show when={selectedItem() === props.item.id}>
+								<div>
+									<DropdownMenu placement="bottom-end">
+										<DropdownMenuTrigger
+											as={Button}
+											variant="ghost"
+											size="icon"
+											class="text-accent hover:text-accent-foreground max-h-8"
+										>
+											<div class="i-mdi:dots-vertical w-1.25em h-1.25em"></div>
+											<span class="sr-only">More</span>
+										</DropdownMenuTrigger>
+										<DropdownMenuContent>
+											<DropdownMenuItem onSelect={() => props.handleDuplicateItem(props.item)}>
+												<div class="i-octicon:duplicate-16 w-1.25em h-1.25em mr-2"></div>
+												Duplicate Element
+											</DropdownMenuItem>
+											<DropdownMenuItem onSelect={() => console.log("Clear Prompt")}>
+												<div class="i-icon-park-outline:clear-format w-1.25em h-1.25em mr-2"></div> Clear Prompt
+											</DropdownMenuItem>
+											<DropdownMenuItem onSelect={() => props.handleDeleteItem(props.item.group, props.item.id)}>
+												<div class="i-octicon:repo-deleted-16 w-1.25em h-1.25em mr-2"></div> Delete Element
+											</DropdownMenuItem>
+											<DropdownMenuItem
+												onSelect={() => {
+													handlePinToggle()
+
+													toast(
+														<div class="flex items-center gap-2">
+															<div class="i-material-symbols:check-box w-4 h-4 text-success" />
+															<span class="text-xs font-medium">
+																{props.item.name} {isPinned() ? "Unpinned" : "Pinned"}
+															</span>
+														</div>,
+														{ duration: 2000, position: "bottom-center" }
+													)
+												}}
+											>
+												<div
+													class={cn(
+														" w-1.25em h-1.25em mr-2",
+														isPinned() && "i-material-symbols-light:toggle-on w-5 h-5",
+														!isPinned() && "i-material-symbols-light:toggle-off-outline w-5 h-5"
+													)}
+												></div>{" "}
+												<Show when={isPinned()}>Unpin Element</Show>
+												<Show when={!isPinned()}>Pin Element</Show>
+											</DropdownMenuItem>
+
+											<DropdownMenuSub overlap>
+												<DropdownMenuSubTrigger>
+													<div class="i-solar:download-linear w-1.25em h-1.25em mr-2" />
+													Move Element
+												</DropdownMenuSubTrigger>
+												<DropdownMenuPortal>
+													<DropdownMenuSubContent>
+														<Select
+															onChange={(e: any) => handleNewGroup(e)}
+															options={groupOptions}
+															optionValue={"value" as any}
+															optionTextValue={"label" as any}
+															placeholder="Move to Group..."
+															itemComponent={props => <SelectItem item={props.item}>{(props.item as any).rawValue.label}</SelectItem>}
+														>
+															<SelectTrigger
+																aria-label="Group"
+																class="w-[180px]"
+															>
+																<SelectValue<{ value: string; label: string }>>
+																	{state => state.selectedOption()?.label || "Move to Group..."}
+																</SelectValue>
+															</SelectTrigger>
+															<SelectContent />
+														</Select>
+													</DropdownMenuSubContent>
+												</DropdownMenuPortal>
+											</DropdownMenuSub>
+										</DropdownMenuContent>
+									</DropdownMenu>
+								</div>
+							</Show>
 						</div>
 					</div>
 				</div>
--- src/components/elements-container.tsx
+++ src/components/elements-container.tsx
@@ -1,10 +1,12 @@
-import { Accessor, createSignal, For, Show } from "solid-js"
+import { For, Show, createEffect, createMemo, Accessor } from "solid-js"
+import { createSignal } from "solid-js"
 import { createStore, produce } from "solid-js/store"
-import Elements from "./elements"
+import { ReactiveSet } from "@solid-primitives/set"
 import { setIsEditingItem, searchSelectedBadges, groupBadge, selected } from "~/global_state"
-import { changeItemAttributes, deleteItem, duplicateItem, moveItemToGroup } from "~/helpers/actionHelpers"
-import { ElementID, GroupID, PromptItem, VersionID } from "~/types/entityType"
-import ItemsCompact from "./elements-compact"
+import { deleteItem, duplicateItem, changeItemAttributes, moveItemToGroup } from "~/helpers/actionHelpers"
+import { PromptItem, ElementID, GroupID, VersionID } from "~/types/entityType"
+import Elements from "./elements"
+import ElementsCompact from "./elements-compact"
 
 interface ElementsContainerProps {
 	type: "all" | "unread"
@@ -16,25 +18,13 @@ interface ElementsContainerProps {
 	setIsFullElements: (value: boolean) => void
 }
 
-const handleUpdateAttributes = (
-	item: PromptItem,
-	name: string,
-	summary: string,
-	body: string,
-	version: VersionID,
-	versionCounter: VersionID,
-	updatedBody: boolean
-) => {
-	if (item.id) {
-		changeItemAttributes(item.group, item.id, name, summary, body, version, versionCounter, updatedBody)
-	}
-}
-
 export default function ElementsContainer(props: ElementsContainerProps) {
-	const handleDeleteItem = (groupId: GroupID, itemId: ElementID) => {
-		const [isLoading, setIsLoading] = createSignal(true)
-		const [items, setItems] = createStore(props.items)
+	const [isLoading, setIsLoading] = createSignal(true)
 
+	const [labelLimit, setLabelLimit] = createSignal<number>(18)
+	const [items, setItems] = createStore(props.items)
+
+	const handleDeleteItem = (groupId: GroupID, itemId: ElementID) => {
 		setItems(
 			produce(items => {
 				const index = items().findIndex(item => item.id === itemId)
@@ -44,17 +34,6 @@ export default function ElementsContainer(props: ElementsContainerProps) {
 			})
 		)
 		deleteItem(groupId, itemId)
-	}
-
-	const handleEditing = (
-		item: PromptItem,
-		label: "title" | "summary" | "body",
-		status: "saved" | "editing",
-		id: string
-	) => {
-		if (item.id) {
-			setIsEditingItem({ label, status, id: id as unknown as ElementID })
-		}
 	}
 
 	const duplicateItemHandler = (item: PromptItem) => {
@@ -69,33 +48,194 @@ export default function ElementsContainer(props: ElementsContainerProps) {
 		}
 	}
 
+	const handleEditing = (
+		item: PromptItem,
+		label: "title" | "summary" | "body",
+		status: "saved" | "editing",
+		id: string
+	) => {
+		if (item.id) {
+			setIsEditingItem({ label, status, id: id as unknown as ElementID })
+		}
+	}
+
+	const handleUpdateAttributes = (
+		item: PromptItem,
+		name: string,
+		summary: string,
+		body: string,
+		version: VersionID,
+		versionCounter: VersionID,
+		updatedBody: boolean
+	) => {
+		if (item.id) {
+			changeItemAttributes(item.group, item.id, name, summary, body, version, versionCounter, updatedBody)
+		}
+	}
+
+	createEffect(() => {
+		if (props.items().length > 0) {
+			setIsLoading(false)
+		}
+	})
+
+	const filterElements = new ReactiveSet<ElementID>()
+
+	createEffect(() => {
+		const selectedGroup = selected() as GroupID
+		const selectedBadges = searchSelectedBadges()
+
+		filterElements.clear()
+		setVisibleCount(500) ///////Here
+
+		selectedBadges.forEach(badge => {
+			const groupBadges = groupBadge.get(selectedGroup)
+			groupBadges?.get(badge.id)?.forEach(id => {
+				filterElements.add(id)
+			})
+		})
+	})
+
+	const [visibleCount, setVisibleCount] = createSignal(0)
+	const [visibleItems, setVisibleItems] = createSignal<PromptItem[]>([])
+
+	const incrementalRender = 25
+
+	const filteredItems = createMemo(() => {
+		const items = props.items()
+		const filters = Array.from(filterElements.values()).flat()
+		return items.filter(item => !item.pinned && (filters.length === 0 || filters.includes(item.id))).reverse()
+	})
+
+	createEffect(() => {
+		setVisibleItems(filteredItems().slice(0, visibleCount()))
+	})
+	const handleScroll = (e: Event) => {
+		const target = e.target as HTMLDivElement
+		if (target.scrollHeight - target.scrollTop <= target.clientHeight + 100) {
+			setVisibleCount(prev => Math.min(prev + incrementalRender, filteredItems().length))
+		}
+	}
+
 	return (
-		<div class="flex min-h-[400px] h-[calc(100vh-280px)] flex-col gap-2 overflow-auto p-4 pt-2 scrollbar-default scrollbar-gutter">
-			<Show when={props.items()}>
-				<For each={props.items()}>
-					{item => {
-						return (
-							<Show
-								when={props.isFullElements() === true}
-								fallback={<ItemsCompact item={item} />}
-							>
-								<div>
-									<Elements
+		<div
+			onScroll={handleScroll}
+			onChange={handleScroll}
+			class="flex min-h-[400px] h-[calc(100vh-280px)] flex-col gap-2 overflow-auto p-4 pt-2 scrollbar-default scrollbar-gutter"
+		>
+			<Show when={!isLoading()}>
+				<Show
+					when={props.isFullElements() === true}
+					fallback={
+						<For
+							each={props
+								.items()
+								.filter(item => item.pinned)
+								.reverse()}
+						>
+							{item => {
+								return (
+									<ElementsCompact
 										item={item}
+										handleEditing={handleEditing}
 										handleUpdateAttributes={handleUpdateAttributes}
 										handleDeleteItem={handleDeleteItem}
 										handleDuplicateItem={duplicateItemHandler}
+										labelLimit={labelLimit}
 										handleMoveItem={moveItemToNewGroup}
-										handleEditing={handleEditing}
-										labelLimit={() => 18}
-										items={props.items()}
+										items={items()}
 										sizes={props.sizes}
+										setIsFullElements={props.setIsFullElements}
 									/>
-								</div>
-							</Show>
-						)
-					}}
-				</For>
+								)
+							}}
+						</For>
+					}
+				>
+					<For
+						each={props
+							.items()
+							.filter(item => item.pinned)
+							.reverse()}
+					>
+						{item => {
+							return (
+								<Elements
+									item={item}
+									handleEditing={handleEditing}
+									handleUpdateAttributes={handleUpdateAttributes}
+									handleDeleteItem={handleDeleteItem}
+									handleDuplicateItem={duplicateItemHandler}
+									labelLimit={labelLimit}
+									handleMoveItem={moveItemToNewGroup}
+									items={items()}
+									sizes={props.sizes}
+								/>
+							)
+						}}
+					</For>
+				</Show>
+			</Show>
+			<Show
+				when={!isLoading() && visibleItems().length > 0 && props.initializedUserElement() === true}
+				fallback={
+					<Show
+						when={selected() !== null && selected() !== undefined && props.initializedUserGroup() === true}
+						fallback={
+							<div class="flex flex-col items-center justify-center h-full">
+								<div class="text-2xl font-bold mb-4">No group selected</div>
+								<div class="text-gray-600 mb-6">Create a new group to get started!</div>
+							</div>
+						}
+					>
+						<div class="flex flex-col items-center justify-center h-full">
+							<div class="text-2xl font-bold mb-4">No items yet</div>
+							<div class="text-gray-600 mb-6">Create your first item to get started!</div>
+						</div>
+					</Show>
+				}
+			>
+				<Show
+					when={props.isFullElements() === true}
+					fallback={
+						<For each={visibleItems()}>
+							{item => {
+								return (
+									<ElementsCompact
+										item={item}
+										handleEditing={handleEditing}
+										handleUpdateAttributes={handleUpdateAttributes}
+										handleDeleteItem={handleDeleteItem}
+										handleDuplicateItem={duplicateItemHandler}
+										labelLimit={labelLimit}
+										handleMoveItem={moveItemToNewGroup}
+										items={items()}
+										sizes={props.sizes}
+										setIsFullElements={props.setIsFullElements}
+									/>
+								)
+							}}
+						</For>
+					}
+				>
+					<For each={visibleItems()}>
+						{item => {
+							return (
+								<Elements
+									item={item}
+									handleEditing={handleEditing}
+									handleUpdateAttributes={handleUpdateAttributes}
+									handleDeleteItem={handleDeleteItem}
+									handleDuplicateItem={duplicateItemHandler}
+									labelLimit={labelLimit}
+									handleMoveItem={moveItemToNewGroup}
+									items={items()}
+									sizes={props.sizes}
+								/>
+							)
+						}}
+					</For>
+				</Show>
 			</Show>
 		</div>
 	)
